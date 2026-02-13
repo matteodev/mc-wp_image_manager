8264a570de187afff3a6b3052670a28ecd9dee0a
matteodev <matteopapparella@gmail.com>
2026-02-13 10:49:12 +0100

    Preparazione plugin: hooks install e uninstall

---
+++ wp-content/plugins/wp-image-manager/class/class-image-manager.php
@@ -0,0 +1,10 @@
+<?php
+class Image_Manager {
+    public function init() {
+       
+    }
+    
+    public function install() {}
+
+    public function uninstall() {}
+}
---
+++ wp-content/plugins/wp-image-manager/image-manager.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: WP Image Manager
+ * Plugin URI: https://github.com/matteodev/wp-image-manager
+ * Description:Realizzazione di una web application di pubblicazione delle immagini.
+ * Version: 1.0.0
+ * Author: Matteo Papparella
+ * Author URI: https://matcode.dev
+ * License: GPL2
+ * Text Domain: mc-wp-image-manager
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+require_once __DIR__ . '/class/class-image-manager.php';
+$image_manager = new Image_Manager();
+$image_manager->init();
+
+//Hooks per installazione e disinstallazione
+register_activation_hook( __FILE__, array( $image_manager, 'install' ) );
+register_deactivation_hook( __FILE__, array( $image_manager, 'uninstall' ) );
