8c07fccb0a093cf5123d4ef5c6f6352a650fce3a
matteodev <matteopapparella@gmail.com>
2026-02-13 12:35:15 +0100

    FIX - $wpdb

--- wp-content/plugins/wp-image-manager/class/class-image-manager.php
+++ wp-content/plugins/wp-image-manager/class/class-image-manager.php
@@ -3,8 +3,10 @@ class Image_Manager {
     private $image_dir_name;
     private $table_name;
     private $table_name_exclude;
+    private $db;
     function __construct() {
         global $wpdb;
+        $this->db = $wpdb;
         $this->image_dir_name = uniqid('mc-images-'); //Repository immagini con chiave unica
         $this->table_name = $wpdb->prefix . 'mc_images';
         $this->table_name_exclude = $wpdb->prefix . 'mc_images_exclude';
@@ -14,13 +16,14 @@ class Image_Manager {
     }
 
     public function install() {
-        $charset_collate = $wpdb->get_charset_collate();
+        $charset_collate = $this->db->get_charset_collate();
         //Preparazione della tabella per le immagini
         $sql_tb_images = "CREATE TABLE $this->table_name (
             id mediumint(9) NOT NULL AUTO_INCREMENT,
             title tinytext NOT NULL,
             description text NOT NULL,
             image_url varchar(255) NOT NULL,
+            metadati json NOT NULL,
             created_at datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
             PRIMARY KEY  (id)
         ) $charset_collate;";
@@ -50,9 +53,9 @@ class Image_Manager {
 
     public function uninstall() {
         //Eliminazione tabella immagini
-        $wpdb->query("DROP TABLE IF EXISTS $this->table_name");
+        $this->db->query("DROP TABLE IF EXISTS $this->table_name");
         //Eliminazione tabella immagini escluse
-        $wpdb->query("DROP TABLE IF EXISTS $this->table_name_exclude");
+        $this->db->query("DROP TABLE IF EXISTS $this->table_name_exclude");
         //Eliminazione repository immagini
         $upload_dir = wp_upload_dir();
         $image_dir = $upload_dir['basedir'] . '/' . $this->image_dir_name;
